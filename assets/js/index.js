25e31830f1a42648afafbba4b9f2a3c75b2e9652
Luis Salcedo <luis.salcedo0906@gmail.com>
2022-07-22 08:45:26 -0500

    video section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
-    
+
+
+    // Click event - mobile menu
+    const mobile_menu_options = document.getElementById('mobile-menu-options-list');
     document.getElementById('hamburguer-botton').addEventListener('click', function(){
-        const mobile_menu_options = document.getElementById('mobile-menu-options-list')
+        
         if(mobile_menu_options.classList.contains('display')){
             mobile_menu_options.classList.remove('display');
         }
